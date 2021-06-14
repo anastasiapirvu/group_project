bc5174072538a14da473157c621529d5b8ede3aa
Anastasia Pirvu <anastasia.pirvu@gmail.com>
2021-06-14 17:30:03 +0200

    Ingredient search component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import IngredientSearch from './IngredientSearch'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h2>I am Hungry</h2>
+    <IngredientSearch/>
     </div>
   );
 }
