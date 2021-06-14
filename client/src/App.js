9c9072cc3268215a3bc6480faff9f5fba16ec322
Abi-Fitz <annyjosh.aj@gmail.com>
2021-06-14 18:03:20 +0000

    favorites component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+
 
 import IngredientSearch from './IngredientSearch'
 
-import './App.css';
+
 import APIfetch from './APIfetch.js'
 import ShoppingList from './components/ShoppingList';
+import React, { useState } from "react";
+import './App.css';
+import Favorites from "./components/Favorites";
+import Recipies from "./components/Recipies";
+
 
 function App() {
+
+  //Hooks
+  const [isFavorites, setIsFavorites] = useState(true)
+  const [isRecipie, setIsRecipie] = useState(false)
+
+
+ const handleChangeView = (isFavorites) => {
+  setIsFavorites(isFavorites)
+}
+
   return (
     <div className="App">
     <h2>I am Hungry</h2>
@@ -20,8 +35,23 @@ function App() {
     <APIfetch />
 
     <ShoppingList />
+    <nav>
+      <button className={isFavorites? "active" : null} onClick={() => handleChangeView(true)}>Favorites</button>
+      <button className={!isFavorites? "active" : null} onClick={() => handleChangeView(false)}>Recipie</button>
+      </nav>
+
+
+
+    { isFavorites ? <Favorites /> 
+    : <Recipies />
+  }
+      
+
     </div>
   );
+
+ 
+  
 }
 
 export default App;
---
+++ client/src/components/Recipies.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+
+
+function Recipies (){
+
+
+    return (
+        <div className="Recipies">
+
+          
+        </div>
+            
+        )
+
+    }        
+
+
+export default Recipies;
