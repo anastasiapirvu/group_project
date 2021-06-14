7f94c06aa22ae13472cc856543d8a4f76a5b5372
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-06-14 19:03:55 +0100

    Shopping List

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ShoppingList from './ShoppingList'
 
 function App() {
   return (
     <div className="App">
     <h2>I am Hungry</h2>
+    <ShoppingList />
     </div>
   );
 }
