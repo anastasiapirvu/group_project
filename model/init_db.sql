4fde4982713690780e188e235dc7ee11f09e9e46
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-06-17 20:27:45 +0100

    Database 2 created

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,11 +1,21 @@
 
 -- (Re)create the table
 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+);
+
 DROP TABLE IF EXISTS items;
 CREATE TABLE items(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
-    quantity INT NOT NULL
+    quantity INT NOT NULL,
+    unit VARCHAR(20) NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE,
 );
 
 -- Insert some test data
