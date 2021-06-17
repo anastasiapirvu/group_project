63240c9e48f77a5b33b1a9c26990bee0df441222
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-06-17 20:57:10 +0100

    Database helper initialises linked databases

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,28 +1,32 @@
 
 -- (Re)create the table
-
+DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS users;
+
+
 CREATE TABLE users(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL
 );
 
-DROP TABLE IF EXISTS items;
+INSERT INTO users (first_name, last_name)
+VALUES
+('Leslie', 'Knope');
+    
+
 CREATE TABLE items(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
     quantity INT NOT NULL,
     unit VARCHAR(20) NOT NULL,
     user_id INT NOT NULL,
-    FOREIGN KEY (users_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT
 );
 
 -- Insert some test data
 
-INSERT INTO users (first_name, last_name)
-VALUES
-    ('Leslie', 'Knope');
+
 
 
 INSERT INTO items (name, quantity, unit, user_id)
