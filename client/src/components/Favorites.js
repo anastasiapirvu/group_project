27a5159b9e2f7911a817bff2018ab4cbd47b6a44
Abi-Fitz <annyjosh.aj@gmail.com>
2021-06-20 21:45:06 +0000

    closing favorites component

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,20 +1,47 @@
+/*
 import React, { useState } from 'react';
 import './Favorites.css';
+import Recipies from './Recipies'; //after repies have been passed
+//import topThree from './topThree';
 
 
 
-function Favorites (){
+//function Favorites (props){
 
 
-    return (
-        <div className="Favorites">
+getFavorites = async () => {
+    try {
+        const results = await fetch("api/favorite/all", { //fetch all favorites
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": localStorage.getItem("token")
+        },
+    
+     });
+
+
+    //render () {
+        const { favorites } = this.state;
+        return (
+            <div className="Favorites">
 
             <h1>Add your Favorite recipie!</h1>
+            {}
+            <IngredientSearchItem
+                    key={favorites.id}
+                    id={favorites.recipiesId}
+                    image={favorites.recipiesImage}
+                    />
         </div>
             
-        )
+     );
+          
+          
+    }   
 
-    }        
+} 
 
 
 export default Favorites;
+*/
