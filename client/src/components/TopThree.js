8f4ae469d9352472202708acd0206d6905bb1417
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-06-21 19:44:56 +0100

    topThree added from starter data

--- client/src/components/TopThree.js
+++ client/src/components/TopThree.js
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 import './TopThree.css';
+import starterData from './topThreeStarter.js'
+
 
 
 
 function TopThree(props){
 
-  const [topThree, setTopThree] = useState([
-    {
-      title: "Apricot Glazed Apple Tart"
-    },
-    {
-      title: "Apple Turnover"
-    },
-    {
-    title: "Apple Donut"
-    },
-      ])
+  const [topThree, setTopThree] = useState(starterData)
+
+  function missedList(recipe){
+    let missed = [];
+    for (let i = 0; i<recipe.missedIngredients.length; i++){
+      missed.push(recipe.missedIngredients[i]["name"])
+    }
+    return missed;
+  }
+
 
     return (
         <div className="TopThree">
           <h2>Your Top Three Suggestions</h2>
-        {topThree.map(t =>(
-          <li key ={t.title}>{t.title}</li>
+        {topThree.map(t => (
+          <li key ={t.id}>{t.title}
+          <br/><img src ={t.image}/>
+            <ul><li className = 'subList' key ={t.title}>
+              <p>Missing Ingredients: {missedList(t).map(m => m+', ')}</p>
+            </li></ul>
+          </li>
         )
         )}
 
