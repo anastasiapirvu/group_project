a4bcd334c3bfe44cda1f9ff9f350c29d58dc7327
Anastasia Pirvu <anastasia.pirvu@gmail.com>
2021-06-24 09:06:27 +0200

    Started with react router

---
+++ client/src/components/Routes.js
@@ -0,0 +1,25 @@
+// import React from "react";
+
+// function Routes(props) {
+//   return (
+//     <Switch>
+//     //Home route
+//     <Route path="/" exact>
+//     </Route>
+
+//     //Shopping List route
+//     <Route path="/shopping-list">
+//     </Route>
+
+//     //Login route
+//     <Route path="/login" exact>
+//     </Route>
+
+//     </Switch>
+
+//   )
+
+
+// }
+
+// export default Routes;
