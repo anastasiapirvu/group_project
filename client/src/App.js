b663131fb2d876b42f066fbd65da3231c8f9958e
Holly L <holly.lyford@gmail.com>
2021-06-25 10:15:04 +0100

    fixing rebase

--- client/src/App.js
+++ client/src/App.js
@@ -59,6 +59,13 @@ function App() {
     history.push("/");
   }
 
+  const [featRecipe, setFeatRecipe] = useState()
+
+  function setFeatId(id){
+    let ix = topThree.findIndex(t => (t.id ===id));
+    setFeatRecipe(topThree[ix]);
+  }
+  
   return (
     <div className="App" style={{ backgroundColor: "#DDFFBC" }}>
       {/* <NavBar user={user} onLogoshut={doLogout} /> */}
